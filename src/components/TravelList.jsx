f66f5af3ce9404a1bb7b6503da3c4dd257e843ac
James Myles <myles.james@gmail.com>
2024-04-02 17:22:38 +0200

    Finished Lab

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
 
 function TravelList() {
+  const [place, setPlace] = useState(travelPlansData);
+
+  const deleteListing = (listingId) => {
+    console.log("It's being called");
+    const copy = JSON.parse(JSON.stringify(place));
+    const filteredListings = copy.filter((thelisting) => {
+      return thelisting.id !== listingId;
+    });
+    setPlace(filteredListings);
+  };
   return (
     <div className="list">
-      {travelPlansData.map((listing) => {
+      {place.map((listing) => {
         return (
-          <>
+          <div key={listing.id}>
             <img src={listing.image} />
             <p>
               {listing.destination} {listing.days} days
@@ -18,8 +29,13 @@ function TravelList() {
             {listing.totalCost > 1500 && <p>Premium</p>}
             {listing.totalCost < 350 && <p>Great Deal!</p>}
             {listing.allInclusive === true && <p>All-Inclusive</p>}
-            <button>Delete</button>
-          </>
+            <button
+              onClick={() => deleteListing(listing.id)}
+              className="btn-delete"
+            >
+              Delete
+            </button>
+          </div>
         );
       })}
     </div>
